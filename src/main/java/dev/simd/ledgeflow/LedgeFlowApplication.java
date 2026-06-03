885700b0e2ca7bb6e2b556bebc9ba9500c95d447
simddev <175357996+simddev@users.noreply.github.com>
2026-06-03 14:31:58 +0200

    Initialize project with Spring Boot 4.0.6

---
+++ src/main/java/dev/simd/ledgeflow/LedgeFlowApplication.java
@@ -0,0 +1,13 @@
+package dev.simd.ledgeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgeFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LedgeFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/simd/ledgeflow/LedgeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simd.ledgeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LedgeFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

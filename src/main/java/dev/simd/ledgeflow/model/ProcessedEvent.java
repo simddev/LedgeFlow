f41a0fd20a0f1c08a2913e94efd54fbe545714d0
simddev <175357996+simddev@users.noreply.github.com>
2026-06-04 18:41:34 +0200

    Add Lombok annotations to ProcessedEvent

--- src/main/java/dev/simd/ledgeflow/model/ProcessedEvent.java
+++ src/main/java/dev/simd/ledgeflow/model/ProcessedEvent.java
@@ -1,10 +1,15 @@
 package dev.simd.ledgeflow.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "processed_events")
 public class ProcessedEvent {

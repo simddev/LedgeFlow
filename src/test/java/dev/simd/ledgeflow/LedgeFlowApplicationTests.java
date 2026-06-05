aeb916636e97b4a9bb94506c2f5f8221626cbf40
simddev <175357996+simddev@users.noreply.github.com>
2026-06-05 12:08:08 +0200

    Add embedded Kafka for context load test

--- src/test/java/dev/simd/ledgeflow/LedgeFlowApplicationTests.java
+++ src/test/java/dev/simd/ledgeflow/LedgeFlowApplicationTests.java
@@ -2,8 +2,10 @@ package dev.simd.ledgeflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 
 @SpringBootTest
+@EmbeddedKafka(partitions = 1, topics = {"account.events", "account.alerts"})
 class LedgeFlowApplicationTests {
 
     @Test

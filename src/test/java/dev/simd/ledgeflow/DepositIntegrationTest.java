d1cb2312421f82ebf740679067fcce540731e810
simddev <175357996+simddev@users.noreply.github.com>
2026-06-06 18:26:43 +0200

    Fix integration test: Flyway priority and Streams state dir conflict

--- src/test/java/dev/simd/ledgeflow/DepositIntegrationTest.java
+++ src/test/java/dev/simd/ledgeflow/DepositIntegrationTest.java
@@ -6,6 +6,7 @@ import dev.simd.ledgeflow.service.AccountService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.KafkaContainer;
@@ -21,8 +22,9 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.flyway.enabled=true")
 @Testcontainers(disabledWithoutDocker = true)
+@DirtiesContext
 class DepositIntegrationTest {
 
     @Container
@@ -38,7 +40,6 @@ class DepositIntegrationTest {
         registry.add("spring.datasource.password", postgres::getPassword);
         registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
         registry.add("spring.jpa.database-platform", () -> "org.hibernate.dialect.PostgreSQLDialect");
-        registry.add("spring.flyway.enabled", () -> "true");
         registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
     }
 
